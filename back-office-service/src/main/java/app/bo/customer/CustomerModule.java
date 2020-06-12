39c799b062486e0b012d61319b680bef64a5b3ab
simple <simpleyu@chancetop.com>
2020-06-12 19:43:03 +0800

    JT-113：JT-123 ：core-ng-api

--- back-office-service/src/main/java/app/bo/customer/CustomerModule.java
+++ back-office-service/src/main/java/app/bo/customer/CustomerModule.java
@@ -1,5 +1,6 @@
 package app.bo.customer;
 
+import app.api.BOCustomerWebService;
 import app.bo.customer.service.BOCustomService;
 import app.bo.customer.web.CustomAjaxWebServiceImpl;
 import core.framework.module.Module;
@@ -15,5 +16,6 @@ public class CustomerModule extends Module {
 
         http().httpPort(8080);
         api().service(CustomerAjaxWebService.class,bean(CustomAjaxWebServiceImpl.class));
+        api().client(BOCustomerWebService.class, "localhost:8080");
     }
 }
