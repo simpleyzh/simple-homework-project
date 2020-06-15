d6580ba9ba809889d9d511af70e8f24968bf1778
simple <simpleyu@chancetop.com>
2020-06-15 18:44:27 +0800

    JT-113：JT-124：core-ng async

--- demo-async/src/main/java/app/demo/product/service/ProductService.java
+++ demo-async/src/main/java/app/demo/product/service/ProductService.java
@@ -5,7 +5,6 @@ import app.demo.api.product.ProductView;
 import app.demo.api.product.UpdateProductRequest;
 import app.demo.api.product.kafka.CreatedProductMessage;
 import app.demo.api.product.kafka.UpdateProductMessage;
-import core.framework.api.web.service.PathParam;
 import core.framework.inject.Inject;
 import core.framework.kafka.MessagePublisher;
 
